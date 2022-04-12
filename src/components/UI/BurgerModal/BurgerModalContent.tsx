7461abc05bc846a9ef33f7972142e22aef92982a
kostekd <dominikkostencki99@gmail.com>
2022-04-12 14:49:29 +0200

    add arrow svg to BurgerModal

--- src/components/UI/BurgerModal/BurgerModalContent.tsx
+++ src/components/UI/BurgerModal/BurgerModalContent.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { BurgerModalProps } from "./BurgerModal";
-import classes from "./BurgerModalContent.module.css";
 
+import classes from "./BurgerModalContent.module.css";
+import BackArrow from "./../../../images/FullLeftArrow.svg";
 //props the same as for BurgerModal.tsx (BurgerModalProps)
 
 const BurgerModalContent = (props: BurgerModalProps) => {
   return (
     <div className={classes["burger-modal"]}>
-      <button onClick={() => props.onClickAction()}>Close this</button>
-      <div className={classes['link-buttons']}>
-        <button><Link to="/add">Add flashcards</Link></button>
+      <button onClick={() => props.onClickAction()}>
+      <img src={BackArrow} alt="Logo" />
+      </button>
+      <div className={classes["link-buttons"]}>
+        <button>
+          <Link to="/add">Add flashcards</Link>
+        </button>
         <button>Log In</button>
         <button>About us</button>
       </div>
