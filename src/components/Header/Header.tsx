1c99f4a3330e593209a9b34ed92179c7cb7fc2cf
kostekd <dominikkostencki99@gmail.com>
2022-04-15 12:39:18 +0200

    add SampleLogo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Burger from "../UI/Burger/Burger";
 import classes from './Header.module.css';
+import BookLogo from './../../images/bookLogo.svg';
 
 const Header = () => {
     return (
         <header className={classes["header-style"]}>
-            <h1>putLearn</h1>
-            <Burger/>
+            <div className={classes.logo}>
+                <img src={BookLogo} alt="Logo" />
+                <h1>putLearn</h1>
+            </div>
+            <Burger />
         </header>
     );
 }
