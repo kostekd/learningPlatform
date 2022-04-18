990c1495fbcacb29e0ac197d25ed277342b5e01d
kostekd <dominikkostencki99@gmail.com>
2022-04-18 21:04:59 +0200

    add dummy testing

--- src/components/Test/Question/Question.tsx
+++ src/components/Test/Question/Question.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 
 interface QuestionProps {
-    onApproveClick : Function,
-    question : {
-        question : String,
-        correct : String,
+    onApproveClick: Function,
+    question: {
+        question: String,
+        correct: String,
         alternative1: String,
         alternative2: String,
     }
 }
 
 
-const Question = (props : QuestionProps) => {
-    return(
+const Question = (props: QuestionProps) => {
+    return (
         <div>
-            
+            <h3>{props.question.question}</h3>
+            <div>{props.question.correct}</div>
+            <div>{props.question.alternative1}</div>
+            <div>{props.question.alternative2}</div>
+            <button onClick={() => props.onApproveClick()}>Confirm</button>
         </div>
     )
 }
